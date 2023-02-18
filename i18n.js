7344d0cfbf3086cd5cad99d440ae3c741ea5baa9
Luca-Pulina <luca.pulina@gmail.com>
2023-02-18 12:22:39 +0100

    feat: add i18n

---
+++ i18n.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './src/locales/en'
+import it from './src/locales/it'
+
+i18n.use(initReactI18next).init({
+    debug: true,
+    fallbackLng: 'en',
+    interpolation: {
+        escapeValue: false, // not needed for react as it escapes by default
+    },
+    // language resources
+    resources: {
+        en,
+        it,
+    },
+})
+
+export default i18n
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { useTranslation } from "react-i18next"
+
 function App() {
-	return <div></div>
+	const { t, i18n } = useTranslation()
+	return (
+		<div>
+			<h1 className='text-2xl text-red-600'> {t("welcome")}</h1>
+		</div>
+	)
 }
 
 export default App
