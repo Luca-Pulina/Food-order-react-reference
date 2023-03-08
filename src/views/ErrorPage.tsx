fefb40cadbd9aea0ed46b76db59f4d7da30988ec
Luca-Pulina <luca.pulina@gmail.com>
2023-03-08 21:52:46 +0100

    feat: return to home after 3 seconds in errorPage

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,8 +1,19 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next"
+import { Link, useNavigate } from "react-router-dom"
 import Header from "../components/Layout/Header"
 
 const ErrorPage = () => {
+	const { t } = useTranslation()
+	const navigate = useNavigate()
+
+	const returnToHomeProgrammatically = () => {
+		setTimeout(() => {
+			navigate("/")
+		}, 3000)
+	}
+
+	returnToHomeProgrammatically()
+
 	return (
 		<div className='w-screen h-screen overflow-y-scroll'>
 			<Header className='w-full h-[10%] border-b-2 border-purple-600  shadow-purple-800 shadow-md' />
@@ -10,7 +21,7 @@ const ErrorPage = () => {
 				<p className='text-white'> Page not found</p>
 				<p className='text-white'>
 					<Link to='/' className=' hover:text-purple-500'>
-						Return to Home
+						{t("GO_TO_HOME")}
 					</Link>
 				</p>
 			</main>
